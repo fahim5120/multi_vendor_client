fd2b8d26fbf0fc32afaca9faed180708cbcd91b0
fahim <muhdfahim786@gamil.com>
2025-12-10 23:10:30 +0530

    designed payment section of sellerdashboard

--- src/seller/Payment/Payment.jsx
+++ src/seller/Payment/Payment.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Card, Divider } from "@mui/material";
+import React from "react";
+import TransactionTable from "../Transaction/TransactionTable";
 
 const Payment = () => {
   return (
-    <div>Payment</div>
-  )
-}
+    <div className="">
+      <div className="space-y-5">
+        <Card className="p-5 rounded-md space-y-4">
+            <h1>Total Earning</h1>
+            <h1 className="font-bold text-xl pb-1">₹12495</h1>
+            <Divider/>
+            <p className="py-2">Last Payment: <strong>₹0</strong></p>
+        </Card>
+        <TransactionTable/>
+      </div>
+    </div>
+  );
+};
 
-export default Payment
+export default Payment;
